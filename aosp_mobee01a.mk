b30c32875fe4f565d15b7a6e60718cab2e956684
Inderpreet Singh <38177112+inderpreetsinghb@users.noreply.github.com>
2018-07-16 14:54:27 +0530

    Update aosp_mobee01a.mk

--- aosp_mobee01a.mk
+++ aosp_mobee01a.mk
@@ -1,5 +1,5 @@
-# Copyright (C) 2014 The CyanogenMod Project
-# Copyright (C) 2018 The LineageOS Project
+#
+# Copyright 2018 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,10 +12,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 $(call inherit-product, device/LYF/mobee01a/full_mobee01a.mk)
 
-# Inherit some common AOSiP stuff.
+# Inherit some common AOSP stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Must define platform variant before including any common things
@@ -28,15 +29,12 @@ PRODUCT_DEVICE := mobee01a
 TARGET_BOOT_ANIMATION_RES := 720
 
 PRODUCT_GMS_CLIENTID_BASE := android-ckt
-TARGET_GAPPS_ARCH := arm64
-TARGET_BOOT_ANIMATION_RES := 720
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="mobee01a_msm8916_64-user 6.0.1 MMB29M 701 release-keys" \
+    PRIVATE_BUILD_DESC="mobee01a_msm8916_64-user 8.1.0 OPM4.171019.021 BN1 release-keys" \
     PRODUCT_NAME=LS-5015 \
     TARGET_DEVICE=LS-5015
 
-BUILD_FINGERPRINT := LYF/LS-5015/LS-5015:6.0.1/MMB29M/434:user/release-keys
+BUILD_FINGERPRINT := LYF/LS-5015/LS-5015:8.1.0/OPM4.171019.021/BN1:user/release-keys
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    DEVICE_MAINTAINERS="Mohd Faraz (AndroiableDroid)"
+
